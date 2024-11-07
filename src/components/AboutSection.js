ded008f7ceaca1aaf333062af572e25090273fea
parthk <pkadakia23@gmail.com>
2024-11-07 15:33:03 -0500

    Final commit v1.0

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -7,14 +7,13 @@ const AboutSection = () => {
     return (
         <div className="about-wrapper">
             <div className="mission-statement">
-                <h3>Our Mission</h3>
+                <h3>My 2 cents..</h3>
                 <p>
-                    At Engraphic Studio, we are committed to delivering exceptional software development, data analysis, and 3D printing solutions with unwavering transparency, quality craftsmanship, and swift execution. We believe in building lasting relationships with our clients by being open, honest, and dedicated to exceeding expectations with every project.
+                    I am dedicated to providing exceptional software development, data analysis, and 3D printing solutions with a focus on transparency, quality craftsmanship, and swift execution. I strive to build lasting relationships by being open, honest, and committed to exceeding expectations in every project I undertake.
                 </p>
             </div>
 
             <div className="team">
-                <h3>Our Team</h3>
                 <div className="team-members">
                     <div className="team-member">
                         <img src={TeamMember1} alt="John Doe" className="team-image" />
